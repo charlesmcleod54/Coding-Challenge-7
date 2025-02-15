678489ac7571c9e328a5a5e7174aa8e50ff9b4d6
Charles McLeod <charlesmcleod@usf.edu>
2025-02-15 17:56:14 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,12 @@ function calculateInvoice(subtotal, taxRate, discount) {
 
 console.log(calculateInvoice(100, 0.08, 5)); // Expected output: "Total Invoice: $103.00"
 console.log(calculateInvoice(500, 0.1, 20)); // Expected output: "Total Invoice: $530.00"
+
+// Task 2
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek *52);
+    return 'Hourly Wage: $${hourlyWage.toFixed(2)}';
+}
+
+console.log(calculateHourlyWage(52000, 40)); // Expected output: "Hourly Wage: $25.00"
+console.log(calculateHourlyWage(75000, 35)); // Expected output: "Hourly Wage: $41.21"
