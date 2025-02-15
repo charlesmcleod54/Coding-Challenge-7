52650f4c5fc270d34d74139985561d1c718b7a5d
Charles McLeod <charlesmcleod@usf.edu>
2025-02-15 18:03:16 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -11,7 +11,17 @@ console.log(calculateInvoice(500, 0.1, 20)); // Expected output: "Total Invoice:
 const calculateHourlyWage = function(salary, hoursPerWeek) {
     let hourlyWage = salary / (hoursPerWeek *52);
     return 'Hourly Wage: $${hourlyWage.toFixed(2)}';
-}
+};
 
 console.log(calculateHourlyWage(52000, 40)); // Expected output: "Hourly Wage: $25.00"
 console.log(calculateHourlyWage(75000, 35)); // Expected output: "Hourly Wage: $41.21"
+
+// Task 3
+const calculateLoyaltyDiscount = (amount, years) => {
+    let discountRate = years >= 5 ? 0.15 : years >= 3 ? 0.10 : 0.05;
+    let discountedPrice = amount * (1 - discountRate);
+    return 'Discounted Price: $${discountedPrice.toFixed(2)}';
+};
+
+console.log(calculateLoyaltyDiscount(100, 6)); // Expected output: "Discounted Price: $85.00"
+console.log(calculateLoyaltyDiscount(200, 2)); // Expected output: "Discounted Price: $190.00"
